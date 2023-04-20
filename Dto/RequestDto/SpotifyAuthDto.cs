c9974f84cfddb977a0829c862269d14cdca99d9a
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-04-20 01:22:08 +0100

    Adding Request Dto

---
+++ Dto/RequestDto/SpotifyAuthDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeedTunes.Dto
+{
+    public class SpotifyAuthDto
+    {
+        public string client_id;
+        public string grant_type;
+        public string client_secret;
+    }
+}
