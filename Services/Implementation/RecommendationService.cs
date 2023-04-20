aaedfae1560155f5687ac0b351f5e41ce9139d8c
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-04-20 01:22:47 +0100

    update recommendation service

--- Services/Implementation/RecommendationService.cs
+++ Services/Implementation/RecommendationService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using WeedTunes.Dto;
 using WeedTunes.Services;
+using WeedTunes.Utilities;
 using WeedTunes.Utilities.Helpers;
 
 namespace WeedTunes.Services
@@ -12,6 +15,19 @@ namespace WeedTunes.Services
     {
         public async Task<BaseResponse<IEnumerable<RecommendationDto>>> Recommend(GetRecommendationDto getRecommendation)
         {
+            var httpClient = new HttpClient();
+            
+            var authModel = new SpotifyAuthDto
+            {
+                client_id = "myclientId",
+                grant_type = "client_credentials"
+            };
+
+            var response = await httpClient
+                .Post("https://accounts.spotify.com/api/token", authModel, "application/x-www-form-urlencoded");
+
+            var authData = await response.ReadContentAs<SpotifyAuthResponseDto>();
+
             return new BaseResponse<IEnumerable<RecommendationDto>>();
         }
     }
