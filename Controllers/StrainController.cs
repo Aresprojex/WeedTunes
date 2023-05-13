d50a00210027323df920926a0b2418a0d7e21a89
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-05-13 11:28:01 +0100

    Add strain controller with create strain endpoint

--- Controllers/StrainController.cs
+++ Controllers/StrainController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WeedTunes.Dto;
+using WeedTunes.Services;
 
 namespace WeedTunes.Controllers
 {
@@ -11,6 +13,17 @@ namespace WeedTunes.Controllers
     [ApiController]
     public class StrainController : BaseController
     {
+        private readonly IStrainService _strainService;
+        public StrainController(IStrainService strainService)
+        {
+            _strainService = strainService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateStrain([FromBody]CreateStrianDto strianDto)
+        {
+            return ReturnResponse(_strainService.CreateStrain(strianDto));
+        }
 
     }
 }
