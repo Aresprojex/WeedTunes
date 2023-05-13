e82e3d5544e6d3b1399b1f8ffc32109b88fb3b10
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-05-13 12:17:19 +0100

    Add mapping profiles

---
+++ Mappings/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using WeedTunes.Dto;
+using WeedTunes.Entities;
+
+namespace WeedTunes.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // maps request dto to entities
+            CreateMap<CreateStrianDto, Strain>();
+            CreateMap<StrainFeelingDto, StrainFeeling>();
+            CreateMap<StrainNegativeAspectDto, StrainNegativeAspect>();
+            CreateMap<StrainHelpsWithDto, StrainHelpsWith>();
+            CreateMap<StrainFlavourDto, StrainFlavour>();
+
+            // maps entities to response dto
+            CreateMap<Strain, StrainDto>();
+
+        }
+    }
+}
