d72b20a28d6452e9730ca09f40544123c5d027fc
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-05-13 12:16:35 +0100

    Add strain feelings and negative aspect models

--- Entities/Strain.cs
+++ Entities/Strain.cs
@@ -5,7 +5,16 @@ using System.Threading.Tasks;
 
 namespace WeedTunes.Entities
 {
-    public class Strain
+    public class Strain : BaseEntity
     {
+        public string Name { get; set; }
+
+        public IEnumerable<StrainFeeling> Feelings { get; set; }
+
+        public IEnumerable<StrainNegativeAspect> NegativeAspects { get; set; }
+
+        public IEnumerable<StrainHelpsWith> HelpsWith { get; set; }
+
+        public IEnumerable<StrainFlavour> Flavours { get; set; }
     }
 }
