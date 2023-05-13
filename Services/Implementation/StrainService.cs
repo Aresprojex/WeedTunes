d7c5d65defe72739d428e2436d556ddcea219537
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-05-13 11:33:41 +0100

    Modify strian service and implementation to return strain dto

--- Services/Implementation/StrainService.cs
+++ Services/Implementation/StrainService.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WeedTunes.Dto;
+using WeedTunes.Utilities.Helpers;
 
 namespace WeedTunes.Services
 {
     public class StrainService : IStrainService
     {
-        public Task CreateStrain(CreateStrianDto strianDto)
+        public Task<BaseResponse<StrainDto>> CreateStrain(CreateStrianDto strianDto)
         {
             throw new NotImplementedException();
         }
--- Services/Interface/IStrainService.cs
+++ Services/Interface/IStrainService.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WeedTunes.Dto;
+using WeedTunes.Utilities.Helpers;
 
 namespace WeedTunes.Services
 {
     public interface IStrainService
     {
         // Creates a strain and it's feeling and negative aspects
-        Task CreateStrain(CreateStrianDto strianDto);
+        Task<BaseResponse<StrainDto>> CreateStrain(CreateStrianDto strianDto);
     }
 }
