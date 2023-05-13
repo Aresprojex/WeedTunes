c61f967bff365d682d6910d0ed3ef803eb53e428
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-05-13 11:14:40 +0100

    Add dbContext

---
+++ Context/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using WeedTunes.Entities;
+
+namespace WeedTunes.Context
+{
+    public class DbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public DbContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Strain> Strains { get; set; }
+
+    }
+}
