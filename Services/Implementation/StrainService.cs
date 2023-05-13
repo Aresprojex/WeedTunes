e23357a0984409f580568f2aee26b526aa472f15
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-05-13 11:25:04 +0100

    Add strain interface and implementation

---
+++ Services/Implementation/StrainService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeedTunes.Dto;
+
+namespace WeedTunes.Services
+{
+    public class StrainService : IStrainService
+    {
+        public Task CreateStrain(CreateStrianDto strianDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Interface/IStrainService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeedTunes.Dto;
+
+namespace WeedTunes.Services
+{
+    public interface IStrainService
+    {
+        // Creates a strain and it's feeling and negative aspects
+        Task CreateStrain(CreateStrianDto strianDto);
+    }
+}
