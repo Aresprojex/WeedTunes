8c9bf715f5e2918cc477ae422648c39b6f262de3
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-07-22 14:05:52 +0100

    Adde strain services

--- Services/Implementation/StrainService.cs
+++ Services/Implementation/StrainService.cs
@@ -1,11 +1,16 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WeedTunes.Data;
 using WeedTunes.Dto;
 using WeedTunes.Entities;
+using WeedTunes.Utilities;
 using WeedTunes.Utilities.Helpers;
+using WeedTunes.Utilities.Pagination;
 
 namespace WeedTunes.Services
 {
@@ -50,6 +55,56 @@ namespace WeedTunes.Services
 
             response.Data = data;
             return response;
+        }
+
+        public async Task<BaseResponse<StrainDto>> GetStrain(Guid id)
+        {
+            var response = new BaseResponse<StrainDto>();
+
+            var strain = await _dbContext.Strains.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(strain == null)
+            {
+                response.Errors.Add("Strain not found");
+                return response;
+            }
+
+
+            response.Data = _mapper.Map<StrainDto>(strain);
+            return response;
+        }
+
+        public async Task<BaseResponse<PagedList<StrainDto>>> GetStrains(BaseSearchViewModel model)
+        {
+            var strains = _dbContext.Strains.AsQueryable();
+
+            var pagedStrain = await EntityFilter(strains, model).ToPagedListAsync(model.PageIndex, model.PageSize);
+
+            var cardsDto = _mapper.Map<IPagedList<Strain>, List<StrainDto>>(pagedStrain);
+
+            var data = new PagedList<StrainDto>(cardsDto, model.PageIndex, model.PageSize, pagedStrain.TotalItemCount);
+
+            return new BaseResponse<PagedList<StrainDto>> { Data = data, TotalCount = data.TotalItemCount, ResponseMessage = $"FOUND {cardsDto.Count} CARD(s)." };
+
+        }
+
+        private IQueryable<Strain> EntityFilter(IQueryable<Strain> query, BaseSearchViewModel model)
+        {
+            if (!string.IsNullOrEmpty(model.Keyword) && !string.IsNullOrEmpty(model.Filter))
+            {
+                switch (model.Filter)
+                {
+                    case "name":
+                        {
+                            return query.Where(x => x.Name.ToLower().Contains(model.Keyword.ToLower()));
+                        }
+
+                    default:
+                        break;
+                }
+            }
+
+            return query;
         }
     }
 }
--- Services/Interface/IStrainService.cs
+++ Services/Interface/IStrainService.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WeedTunes.Dto;
+using WeedTunes.Utilities;
 using WeedTunes.Utilities.Helpers;
+using WeedTunes.Utilities.Pagination;
 
 namespace WeedTunes.Services
 {
@@ -11,5 +13,7 @@ namespace WeedTunes.Services
     {
         // Creates a strain and it's feeling and negative aspects
         Task<BaseResponse<StrainDto>> CreateStrain(CreateStrianDto strianDto);
+        Task<BaseResponse<StrainDto>> GetStrain(Guid id);
+        Task<BaseResponse<PagedList<StrainDto>>> GetStrains(BaseSearchViewModel search);
     }
 }
